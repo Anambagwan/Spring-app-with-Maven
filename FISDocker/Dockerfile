b309e7a5a85f0be4719dd9238b65c9c72a90683c
Anam Bagwan <65122751+Anambagwan@users.noreply.github.com>
2024-10-07 14:28:03 +0530

    1st

---
+++ FISDocker/Dockerfile
@@ -0,0 +1,4 @@
+FROM openjdk:17
+EXPOSE 8086
+ADD target/FISDocker-0.0.1-SNAPSHOT.jar FISDocker-0.0.1-SNAPSHOT.jar 
+ENTRYPOINT ["java","-jar","/FISDocker-0.0.1-SNAPSHOT.jar"]
---
+++ FISDocker/src/test/java/com/demo/FISDocker/FisDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.FISDocker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FisDockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
